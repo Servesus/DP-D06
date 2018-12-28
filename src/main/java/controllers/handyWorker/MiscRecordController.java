5980715a5a88a49ce10fc41ebc9c43dac14f8f64
Servesus <jesusgarrocho1998@gmail.com>
2018-12-28 20:50:41 +0100

    miscRecord controller casi terminado falta testear

--- src/main/java/controllers/handyWorker/MiscRecordController.java
+++ src/main/java/controllers/handyWorker/MiscRecordController.java
@@ -3,10 +3,15 @@ package controllers.handyWorker;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.ActorService;
@@ -41,10 +46,78 @@ public class MiscRecordController extends AbstractController {
 
 		records = hw.getCurricula().getMiscRecord();
 
-		result = new ModelAndView("miscRecord/list");
+		result = new ModelAndView("miscRecord/handyWorker/list");
 		result.addObject("miscRecord", records);
 		result.addObject("requestURI", "miscRecord/handyWorker/list.do");
 
+		return result;
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView result;
+		MiscRecord record;
+
+		record = this.miscRecordService.create();
+		result = this.createEditModelAndView(record);
+
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int recordId) {
+		ModelAndView result;
+		MiscRecord record;
+
+		record = this.miscRecordService.findOne(recordId);
+		Assert.notNull(record);
+		result = this.createEditModelAndView(record);
+
+		return result;
+
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid final MiscRecord miscRecord, final BindingResult binding) {
+		ModelAndView result;
+		if (binding.hasErrors())
+			result = this.createEditModelAndView(miscRecord);
+		else
+			try {
+				this.miscRecordService.save(miscRecord);
+				result = new ModelAndView("redirect:list.do");
+			} catch (final Throwable oops) {
+				result = this.createEditModelAndView(miscRecord, "miscRecord.commit.error");
+			}
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
+	public ModelAndView delete(final MiscRecord record, final BindingResult binding) {
+		ModelAndView result;
+
+		try {
+			this.miscRecordService.delete(record);
+			result = new ModelAndView("redirect:list.do");
+		} catch (final Throwable oops) {
+			result = this.createEditModelAndView(record, "miscRecord.commit.error");
+		}
+		return result;
+	}
+
+	protected ModelAndView createEditModelAndView(final MiscRecord record) {
+		ModelAndView result;
+
+		result = this.createEditModelAndView(record, null);
+
+		return result;
+	}
+
+	protected ModelAndView createEditModelAndView(final MiscRecord record, final String messageCode) {
+		ModelAndView result;
+		result = new ModelAndView("miscRecord/handyWorker/edit");
+		result.addObject("miscRecord", record);
+		result.addObject("message", messageCode);
 		return result;
 	}
 
