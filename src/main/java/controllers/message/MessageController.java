8a42fd936fa5bd897a9c3628613d7242950a0fa1
MrGaRRos <juanmagarrocho@gmail.com>
2019-01-09 17:23:08 +0100

    Empezando delete en message controller

--- src/main/java/controllers/message/MessageController.java
+++ src/main/java/controllers/message/MessageController.java
@@ -3,14 +3,17 @@ package controllers.message;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.ActorService;
 import services.BoxService;
 import services.MessageService;
 import domain.Box;
+import domain.Message;
 
 @Controller
 @RequestMapping("message/customer,handyWorker,referee,administrator")
@@ -22,10 +25,13 @@ public class MessageController {
 	@Autowired
 	BoxService		boxService;
 
+	@Autowired
+	ActorService	actorService;
+
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ModelAndView list(@RequestParam final int boxId) {
-		final ModelAndView result;
+		ModelAndView result;
 		Box box;
 
 		box = this.boxService.findOne(boxId);
@@ -37,4 +43,27 @@ public class MessageController {
 
 		return result;
 	}
+
+	//TODO
+	@RequestMapping(value = "", method = RequestMethod.POST, params = "")
+	public ModelAndView delete(final int boxId, final Message message, final BindingResult binding) {
+		ModelAndView result;
+		Box box;
+
+		box = this.boxService.findOne(boxId);
+
+		try {
+			if (box.getMessages().contains(message) && !(box.getName().equals("trashBox"))) {
+				this.messageService.moveMessage(message, trashBox);
+				result = new ModelAndView("redirect:list.do");
+			} else {
+				this.messageService.delete(message);
+				result = new ModelAndView("redirect:list.do");
+			}
+		} catch (final Throwable oops) {
+			result = this.createEditModelAndView(box, "message.commit.error");
+		}
+		return result;
+	}
+
 }
